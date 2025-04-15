1984a8b903ea611db11cdc2d6394a8185c23578d
alpineee <m.alfin.z117@gmail.com>
2025-04-15 08:48:01 +0700

    fix error

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   transpilePackages: ['@mui/material', '@mui/system', '@mui/icons-material'],
   images: {
     remotePatterns: [
       {
-        protocol: 'data',
+        protocol: 'https',
         hostname: '*',
       },
     ],
